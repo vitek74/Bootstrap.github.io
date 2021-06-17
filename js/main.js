ae10d3da6a1b5de9e9ed6859ed7e1dc2fa8e5ad0
vitek74 <37906484+vitek74@users.noreply.github.com>
2021-06-17 21:02:36 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
+   
+    $(".navbar a").click(function(){
+        $("body,html").animate({
+            scrollTop:$("#" + $(this).data('value')).offset().top
+        },1000)
+     
+    })
+   
    })
+   © 2021 GitHub, Inc.
